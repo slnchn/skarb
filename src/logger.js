8c7c11b8c348bcf5b87f8ddaee296e7661ce01b4
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-08-26 19:43:19 +0200

    add basic logging

---
+++ src/logger.js
@@ -0,0 +1,29 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'skarb.log' }),
+  ],
+});
+
+const decorateWithArgsLogger =
+  (func) =>
+  (...args) => {
+    try {
+      logger.info(`${func.name} called`);
+      const result = func(...args);
+      logger.info(`${func.name} successfully executed`);
+      return result;
+    } catch (error) {
+      logger.error(`${func.name} failed with error: ${error}`);
+      throw error;
+    }
+  };
+
+module.exports = { logger, decorateWithArgsLogger };

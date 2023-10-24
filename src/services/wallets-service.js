3f13ee441d47ae09010f1b00786ee0acafa7d5f6
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2023-10-24 20:37:21 +0200

    release/1.3.1 (#74)
    * 71-move-business-logic-to-the-services-keep-routing-in-controllers (#73)

---
+++ src/services/wallets-service.js
@@ -0,0 +1,62 @@
+// repositories
+const {
+  selectWalletsByNameCaseInsensitive,
+  insertWallet,
+  deleteWalletHard,
+  deleteWalletSoft,
+  selectWallets,
+} = require('../repositories/wallet-repository');
+const { selectCurrencyById } = require('../repositories/currency-repository');
+
+// formatters
+const { formatWalletFromDb } = require('../formatters/wallets-formatter');
+const {
+  selectWalletsHistoryByWalletId,
+} = require('../repositories/whistory-repository');
+
+const addWallet = async (currencyId, wallet) => {
+  const walletsWithSameName = await selectWalletsByNameCaseInsensitive(wallet);
+
+  if (!walletsWithSameName.length) {
+    const [currency] = await selectCurrencyById(currencyId);
+    if (currency) {
+      // create wallet only if the referenced currency already exists
+      const result = await insertWallet({ wallet, currencyId });
+      console.table(result.map(formatWalletFromDb));
+    } else {
+      console.error(`Currency with id ${currencyId} not found!`);
+    }
+  } else {
+    console.error('Wallet name is not unique. Please choose another one.');
+    console.table(walletsWithSameName.map(formatWalletFromDb));
+  }
+};
+
+const removeWallet = async (walletId, hard = false) => {
+  const relatedWhistoryEntries = await selectWalletsHistoryByWalletId(walletId);
+
+  if (!relatedWhistoryEntries.length) {
+    let deletedWallet = {};
+    if (hard) {
+      deletedWallet = await deleteWalletHard(walletId);
+    } else {
+      deletedWallet = await deleteWalletSoft(walletId);
+    }
+
+    console.table(deletedWallet.map(formatWalletFromDb));
+  } else {
+    console.error(`Wallet with id ${walletId} has related whistory entries!`);
+    console.table(relatedWhistoryEntries);
+  }
+};
+
+const listWallets = async () => {
+  const wallets = await selectWallets();
+  console.table(wallets.map(formatWalletFromDb));
+};
+
+module.exports = {
+  addWallet,
+  removeWallet,
+  listWallets,
+};
--- src/services/whistory-service.js
+++ src/services/whistory-service.js
@@ -1,5 +1,26 @@
+const { selectWalletById } = require('../repositories/wallet-repository');
+const {
+  insertWhistory,
+  deleteWalletHistoryHard,
+  deleteWalletHistorySoft,
+  selectWalletHistory,
+  selectWalletsHistory,
+} = require('../repositories/whistory-repository');
+
+// services
 const { getTableCsvContent, exportData } = require('./exporting-service');
+
+// formatters
+const { formatWhistoryFromDb } = require('../formatters/whistory-formatter');
+
+// utils
 const { formatDateToSimpleString } = require('../utils');
+const child = require('child_process');
+const path = require('path');
+const {
+  chunkWhistoryByDays,
+  getWhistorySpanDiff,
+} = require('../utils/whistory-utils');
 
 const exportWhistoryToCsv = async (walletsHistory, walletName) => {
   try {
@@ -16,6 +37,101 @@ const exportWhistoryToCsv = async (walletsHistory, walletName) => {
   }
 };
 
+const addWhistoryEntry = async (walletId, amount, date) => {
+  const [wallet] = await selectWalletById(walletId);
+  if (wallet) {
+    const result = await insertWhistory({ walletId, amount, date });
+    console.table(result.map(formatWhistoryFromDb));
+  } else {
+    console.log(`Wallet with id ${walletId} does not exist`);
+  }
+};
+
+const removeWhistoryEntry = async (walletHistoryId, hard = false) => {
+  let result = {};
+  if (hard) {
+    result = await deleteWalletHistoryHard(walletHistoryId);
+  } else {
+    result = await deleteWalletHistorySoft(walletHistoryId);
+  }
+
+  console.table(result.map(formatWhistoryFromDb));
+};
+
+const listWhistory = async (walletId) => {
+  let result = [];
+  if (walletId) {
+    result = await selectWalletHistory(walletId);
+  } else {
+    result = await selectWalletsHistory();
+  }
+
+  console.table(result.map(formatWhistoryFromDb));
+};
+
+const exportWhistory = async (walletId) => {
+  let result = [];
+  let walletName = '';
+  if (walletId) {
+    result = await selectWalletHistory(walletId);
+    walletName = result[0].w_name;
+  } else {
+    result = await selectWalletsHistory();
+  }
+
+  await exportWhistoryToCsv(result, walletName);
+};
+
+const plotWhistory = async (walletId) => {
+  const walletHistory = await selectWalletHistory(walletId);
+
+  const process = child.spawn('python', [
+    path.join(__dirname, '../../scripts/plot_whistory.py'),
+    JSON.stringify(walletHistory),
+  ]);
+
+  process.stdout.on('data', (data) => {
+    console.log(`Python script output: ${data}`);
+  });
+
+  process.stderr.on('data', (data) => {
+    console.error(`Python script error: ${data}`);
+  });
+
+  process.on('close', (code) => {
+    console.log(`Python script exited with code ${code}`);
+  });
+};
+
+const plotWhistoryDiff = async (walletId, span) => {
+  const walletHistory = await selectWalletHistory(walletId);
+  const formattedWalletHistory = walletHistory.map(formatWhistoryFromDb);
+  const whistoryByDays = chunkWhistoryByDays(formattedWalletHistory, +span);
+  const whistoryDiff = whistoryByDays.map(getWhistorySpanDiff);
+
+  const process = child.spawn('python', [
+    path.join(__dirname, '../../scripts/plot_whistory_diff.py'),
+    JSON.stringify(whistoryDiff),
+  ]);
+
+  process.stdout.on('data', (data) => {
+    console.log(`Python script output: ${data}`);
+  });
+
+  process.stderr.on('data', (data) => {
+    console.error(`Python script error: ${data}`);
+  });
+
+  process.on('close', (code) => {
+    console.log(`Python script exited with code ${code}`);
+  });
+};
+
 module.exports = {
-  exportWhistoryToCsv,
+  addWhistoryEntry,
+  removeWhistoryEntry,
+  listWhistory,
+  exportWhistory,
+  plotWhistory,
+  plotWhistoryDiff,
 };

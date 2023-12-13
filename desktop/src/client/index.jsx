77bbc89685be6deffb202376879ac300a812aeb9
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-12-13 21:41:02 +0100

    add tailwind

--- desktop/src/client/index.jsx
+++ desktop/src/client/index.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+import './index.css';
+
 window.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('root')).render(<App />);
 });

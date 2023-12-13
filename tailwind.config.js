7c27634734db1e6af56c3db38c07e847019aee03
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2023-12-13 21:52:42 +0100

    81-add-tailwind-to-the-desktop-app (#84)

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./desktop/src/client/index.html', './desktop/src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

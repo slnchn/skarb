2e2d76ab9298d832faf7f0b864c7fd28b01a09c3
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-12-14 19:31:17 +0100

    remove dev thing

--- desktop/src/client/App.jsx
+++ desktop/src/client/App.jsx
@@ -5,7 +5,7 @@ import ConnectionPanel from './ConnectionPanel';
 
 const App = () => {
   return (
-    <div className="w-full h-full overflow-auto bg-red-500">
+    <div className="w-full h-full overflow-auto">
       <ConnectionPanel />
     </div>
   );
